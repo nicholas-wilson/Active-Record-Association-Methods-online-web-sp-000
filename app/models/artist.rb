49454e9f9c3c1d2b95dc3fbc10530532b7909091
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-28 22:16:48 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    Song.all.first.genre
+    Artist.songs.first.genre
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    Artist.songs.size
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    Artist.genres.size
   end
 end
